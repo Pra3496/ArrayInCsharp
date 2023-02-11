1563f7691bf215c2554be556808b357a52781aac
Pranav Waghmare <piyushw55@gmail.com>
2023-02-12 01:06:38 +0530

    Length of the Array

---
+++ Arrays_In_Csharp/Arrays.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Arrays_In_Csharp
+{
+    internal class Arrays
+    {
+        int[] arr;
+
+        public Arrays(int[] arr)
+        {
+            
+            this.arr = arr;
+            
+        }
+        public int getLength()
+        {
+            return arr.Length;
+        }
+
+        public void getMaxAndMin()
+        {
+            int iMax=0,iMin=0;
+
+            for(int i = 0 ;i < arr.Length;i++)
+            {
+                if (arr[i] >= iMax)
+                {
+                    iMax = arr[i];
+                }
+                else
+                {
+                    iMin = arr[i];
+                }
+            }
+
+            Console.Write("\nIn Given Array Max number is {0} and Minimum is {1}",iMax,iMin);
+        }
+
+        public void getNegative()
+        {
+
+            int iCnt = 0;
+            for (iCnt = 0; iCnt < arr.Length; iCnt++)
+            {
+                if (arr[iCnt] < 0)
+                {
+                    break;
+                }
+            }
+            if (iCnt == arr.Length)
+            {
+                Console.Write("\nThere No Negative Numbers in Array !!!!!!");
+            }
+            else
+            {
+                Console.Write("\nNegative Numbers are :");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] < 0)
+                    {
+                        Console.Write("\t" + arr[i]);
+                    }
+
+                }
+            }
+        }
+
+        public void getSumOfArray()
+        {
+            int iSum = 0;
+        
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                iSum += arr[i];
+
+            }
+            
+            Console.Write("\nSum of Arry is : " + iSum);
+                 
+        }
+
+
+        public void prntArr()
+        {
+            Console.Write("\nElements are: ");
+            foreach (int i in arr)
+            {
+                Console.Write("\t"+i);
+            }
+        }
+
+        public void getDigitCnt()
+        {
+            for(int i = 0;i < arr.Length; i++)
+            {
+                int iCnt=0,iNo = arr[i];
+               
+                while(iNo != 0)
+                {
+                    iNo/=10;
+                    iCnt++;
+                }
+                Console.Write("\nElement {0} having {1} Digits",i, iCnt);
+                
+            }
+        }
+
+
+        public void sortArray()
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        temp = this.arr[i];
+                        this.arr[i] = this.arr[j];
+                        this.arr[j] = temp;
+                    }
+
+                }
+
+            }
+            Console.Write("\nSorted Array is : ");
+            prntArr();
+
+        }
+
+
+
+    }
+}
---
+++ Arrays_In_Csharp/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Xml.Linq;
+
+namespace Arrays_In_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Declaration of arrays
+            
+            int[] myNum = { 1, 2, 3, 4 };
+            float[] myNum2 = { 1.25F, 0.2F };
+            double[] myNum3 = { 1.0234D, 5.69876D, 5.96321D };
+
+
+            string[] myNam = { "pranav", "anil", "waghmare" };
+
+            //Indexing of array
+
+            Console.WriteLine(myNum[0]);
+            myNum[0] = 77;
+            Console.WriteLine(myNum[0]);
+
+            Console.WriteLine(myNam[0]);
+
+            
+
+            // Create an array of four elements, and add values later
+            string[] cars = new string[4];
+
+            // Create an array of four elements and add values right away 
+            object[] cars1 = new object[] {1,2,3,4,5};
+
+            // Create an array of four elements without specifying the size 
+            string[] cars2 = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
+
+            Console.WriteLine(cars.Length);
+
+
+            //Display Elements of Array
+
+            for (int i = 0; i < cars1.Length; i++)
+            {
+                object o = cars1[i];
+
+                Console.WriteLine();
+            }
+
+
+            
+            foreach (var i in cars1)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            //
+
+            int[] iArr = { 1, -1, 2, 5, 1 };
+
+            Arrays obj = new Arrays(iArr);
+
+
+
+            Console.Write("Elements of Array");
+            obj.prntArr();
+            Console.WriteLine("\n");
+
+            Console.Write("\nLength of Array : ");
+            Console.Write(obj.getLength());
+            Console.WriteLine("\n");
+
+
+            Console.Write("Maximum and Minimum of Array");
+            obj.getMaxAndMin();
+            Console.WriteLine("\n");
+
+            Console.Write("Count of Digits of Element Array");
+            obj.getDigitCnt();
+            Console.WriteLine("\n");
+
+
+            Console.Write("Negative Elements of Array");
+            obj.getNegative();
+            Console.WriteLine("\n");
+
+            Console.Write("Sum of Array");
+            obj.getSumOfArray();
+            Console.WriteLine("\n");
+
+            Console.Write("Sorted Array");
+            obj.sortArray();
+            Console.WriteLine("\n");
+
+
+
+
+
+
+        }
+    }
+}
